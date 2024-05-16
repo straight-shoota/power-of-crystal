8f3cb6c824c1712781bfedfa1802698b82601032
Johannes Müller <straightshoota@gmail.com>
2024-05-16 12:30:14 +0200

    Add `src/`

---
+++ src/http-server.cr
@@ -0,0 +1,11 @@
+require "http/server"
+
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello, Crystal!"
+end
+
+address = server.bind_tcp(8080)
+puts "Listening on http://#{address}"
+
+server.listen
